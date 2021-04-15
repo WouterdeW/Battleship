a459fd019cb565f4f4b6181bb1df3958aeb9ad01
Wauderr <wouterwdewilde@gmail.com>
2021-04-15 20:51:14 +0200

    board set-up

--- BattleShip.py
+++ BattleShip.py
@@ -6,77 +6,130 @@ import Board as board
 # Submarine (s) - 3
 # Destroyer (d) - 2
 
+ships = ["Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"]
+lengths = [5, 4, 3, 3, 2]
 
-def orientation_ship(shipname, length, ship_start):
+
+def orientation_ship(ship_letter, length, ship_start):
     ship_start_letter = ship_start[0]
-    ship_start_idx = board.rows.find(ship_start_letter)
+    ship_start_letter_idx = board.rows.index(ship_start_letter)
     orientation = str(input(
         "Would you like to like the bow to point? North (N), East (E), South (S) or West (W)?\n Please pick a letter: ")).lower()
     while orientation not in ["n", "e", "s", "w"]:
         orientation = str(input("Please select N/E/S/W: ")).lower()
 
-    while not is_possible(ship_start, length, orientation):
+    while not is_on_board(ship_start, length, orientation) or is_overlapping(ship_start, length, orientation):
         orientation = str(input(
-            "Sorry, this is not possible, the ship would be out of bounds.\n Please select N/E/S/W: ")).lower()
+            "Sorry, this is not possible, the ship would be out of bounds or overlapping another ship.\n Please select N/E/S/W: ")).lower()
         while orientation not in ["n", "e", "s", "w"]:
             orientation = str(input("Please select N/E/S/W: ")).lower()
 
     if orientation == 'n':
         for i in range(length):
-            coordinate = board.rows[ship_start_idx - i] + ship_start[1]
-            board.board[coordinate] = shipname
+            coordinate = board.rows[ship_start_letter_idx - i] + ship_start[1]
+            board.board[coordinate] = ship_letter
     elif orientation == 's':
         for i in range(length):
-            coordinate = board.rows[ship_start_idx + i] + ship_start[1]
-            board.board[coordinate] = shipname
+            coordinate = board.rows[ship_start_letter_idx + i] + ship_start[1]
+            board.board[coordinate] = ship_letter
     elif orientation == "e":
         for i in range(length):
             coordinate = ship_start[0] + str(int(ship_start[1]) + i)
-            board.board[coordinate] = shipname
+            board.board[coordinate] = ship_letter
     elif orientation == 'w':
-        coordinate = ship_start[0] + str(int(ship_start[1]) - i)
-        board.board[coordinate] = shipname
+        for i in range(length):
+            coordinate = ship_start[0] + str(int(ship_start[1]) - i)
+            board.board[coordinate] = ship_letter
 
 
-def is_possible(ship_start, length, orientation):
+def is_on_board(ship_start, length, orientation):
     ship_start_letter = ship_start[0]
-    ship_start_letter_idx = board.rows.find(ship_start_letter)
+    ship_start_letter_idx = board.rows.index(ship_start_letter)
     coordinate_numbers = [i for i in range(1, 11)]
     ship_start_number = int(ship_start[1])
-    ship_start_number_idx = coordinate_numbers.find(ship_start_number)
+    ship_start_number_idx = coordinate_numbers.index(ship_start_number)
     if orientation == 'n':
-        try:
-            board.rows[ship_start_letter_idx - (length - 1)]
-        except IndexError:
+        if ship_start_letter_idx - (length - 1) < 0:
             return False
+        # try:
+        #    board.rows[ship_start_letter_idx - (length - 1)]
+        # except IndexError:
+        #    return False
         return True
     elif orientation == 's':
-        try:
-            board.rows[ship_start_letter_idx + (length - 1)]
-        except IndexError:
+        if ship_start_letter_idx + (length - 1) > 9:
             return False
+        # try:
+        #    board.rows[ship_start_letter_idx + (length - 1)]
+        # except IndexError:
+        #    return False
         return True
     elif orientation == "e":
-        try:
-            coordinate_numbers[ship_start_number_idx + (length - 1)]
-        except IndexError:
+        if ship_start_number_idx + (length - 1) > 9:
             return False
+        # try:
+        #    coordinate_numbers[ship_start_number_idx + (length - 1)]
+        # except IndexError:
+        #    return False
         return True
     elif orientation == "w":
-        try:
-            coordinate_numbers[ship_start_number_idx - (length - 1)]
-        except IndexError:
+        if ship_start_number_idx - (length - 1) < 0:
+            return False
+        # try:
+        #    coordinate_numbers[ship_start_number_idx - (length - 1)]
+        # except IndexError:
+        #    return False
+        return True
+
+
+def is_overlapping(ship_start, length, orientation=None):
+    ship_start_letter = ship_start[0]
+    ship_start_letter_idx = board.rows.index(ship_start_letter)
+    if orientation == 'n':
+        for i in range(length):
+            coordinate = board.rows[ship_start_letter_idx - i] + ship_start[1]
+            if board.board[coordinate] != " ":
+                return True
+            return False
+    elif orientation == 's':
+        for i in range(length):
+            coordinate = board.rows[ship_start_letter_idx + i] + ship_start[1]
+            if board.board[coordinate] != " ":
+                return False
+            return True
+    elif orientation == "e":
+        for i in range(length):
+            coordinate = ship_start[0] + str(int(ship_start[1]) + i)
+            if board.board[coordinate] != " ":
+                return False
+            return True
+    elif orientation == 'w':
+        for i in range(length):
+            coordinate = ship_start[0] + str(int(ship_start[1]) - i)
+            if board.board[coordinate] != " ":
+                return False
+            return True
+    else:
+        if board.board[ship_start] == " ":
             return False
         return True
 
 
 def place_ship(shipname, length):
-    ship_start = input(
-        "Where would you like to place the stern of the {0}? Please select one of the coordinates\n").format(shipname)
-    while str(ship_start) not in board.board.keys():
-        ship_start = input("Dude.. do you know how coordinates work? Just pick a valid one.\n")
-    orientation_ship(shipname, length, ship_start)
+    question = "Where would you like to place the stern of the {one}? Please select one of the coordinates\n".format(
+        one=shipname)
+    ship_start = str(input(question)).lower()
+    while ship_start not in board.board.keys() or is_overlapping(ship_start, 1):
+        ship_start = input(
+            "This is not a valid coordinate or this coordinate is already taken. Please select another one\n")
+    orientation_ship(shipname[0], length, ship_start)
 
 
-orientation_ship('c', 2, "b2")
-board.print_board()
+def board_setup():
+    board.print_board()
+    for i in range(5):
+        place_ship(ships[i], lengths[i])
+        board.print_board()
+
+
+board_setup()
