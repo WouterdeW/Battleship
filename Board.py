a6d2df2c58824e8c2172f4626b458762215ab7fc
Wauderr <wouterwdewilde@gmail.com>
2021-04-24 19:18:56 +0200

    Board setup complete. Playing the game has started. Moved the board set-up to Board_Setup.py

--- Board.py
+++ Board.py
@@ -1,56 +1,103 @@
 import string
 
-board = {}
+board_user = {}
+board_cpu = {}
 rows = string.ascii_lowercase[:10]
 
 for letter in rows:
     for i in range(1, 11):
-        board[letter+str(i)] = " "
+        board_user[letter+str(i)] = " "
+        board_cpu[letter+str(i)] = " "
 
 hor_vert = "___|_____|_____|_____|_____|_____|_____|_____|_____|_____|_____"
 vert = "   |     |     |     |     |     |     |     |     |     |     "
 
 
-def print_board():
+def print_board_user():
     print("   |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  | 10  ")
     print(hor_vert)
     print(vert)
-    print(" A |  "+board["a1"]+"  |  "+board["a2"]+"  |  "+board["a3"]+"  |  "+board["a4"]+"  |  "+board["a5"]+"  |  " +
-          board["a6"]+"  |  "+board["a7"]+"  |  "+board["a8"]+"  |  "+board["a9"]+"  | "+board["a10"]+"  ")
+    print(" A |  "+board_user["a1"]+"  |  "+board_user["a2"]+"  |  "+board_user["a3"]+"  |  "+board_user["a4"]+"  |  "+board_user["a5"]+"  |  " +
+          board_user["a6"]+"  |  "+board_user["a7"]+"  |  "+board_user["a8"]+"  |  "+board_user["a9"]+"  |  "+board_user["a10"]+"  ")
     print(hor_vert)
     print(vert)
-    print(" B |  "+board["b1"]+"  |  "+board["b2"]+"  |  "+board["b3"]+"  |  "+board["b4"]+"  |  "+board["b5"]+"  |  " +
-          board["b6"]+"  |  "+board["b7"]+"  |  "+board["b8"]+"  |  "+board["b9"]+"  |  "+board["b10"]+"  ")
+    print(" B |  "+board_user["b1"]+"  |  "+board_user["b2"]+"  |  "+board_user["b3"]+"  |  "+board_user["b4"]+"  |  "+board_user["b5"]+"  |  " +
+          board_user["b6"]+"  |  "+board_user["b7"]+"  |  "+board_user["b8"]+"  |  "+board_user["b9"]+"  |  "+board_user["b10"]+"  ")
     print(hor_vert)
     print(vert)
-    print(" C |  "+board["c1"]+"  |  "+board["c2"]+"  |  "+board["c3"]+"  |  "+board["c4"]+"  |  "+board["c5"]+"  |  " +
-          board["c6"]+"  |  "+board["c7"]+"  |  "+board["c8"]+"  |  "+board["c9"]+"  |  "+board["c10"]+"  ")
+    print(" C |  "+board_user["c1"]+"  |  "+board_user["c2"]+"  |  "+board_user["c3"]+"  |  "+board_user["c4"]+"  |  "+board_user["c5"]+"  |  " +
+          board_user["c6"]+"  |  "+board_user["c7"]+"  |  "+board_user["c8"]+"  |  "+board_user["c9"]+"  |  "+board_user["c10"]+"  ")
     print(hor_vert)
     print(vert)
-    print(" D |  "+board["d1"]+"  |  "+board["d2"]+"  |  "+board["d3"]+"  |  "+board["d4"]+"  |  "+board["d5"] +
-          "  |  " + board["d6"]+"  |  "+board["d7"]+"  |  "+board["d8"]+"  |  "+board["d9"]+"  |  "+board["d10"]+"  ")
+    print(" D |  "+board_user["d1"]+"  |  "+board_user["d2"]+"  |  "+board_user["d3"]+"  |  "+board_user["d4"]+"  |  "+board_user["d5"] +
+          "  |  " + board_user["d6"]+"  |  "+board_user["d7"]+"  |  "+board_user["d8"]+"  |  "+board_user["d9"]+"  |  "+board_user["d10"]+"  ")
     print(hor_vert)
     print(vert)
-    print(" E |  "+board["e1"]+"  |  "+board["e2"]+"  |  "+board["e3"]+"  |  "+board["e4"]+"  |  "+board["e5"] +
-          "  |  " + board["e6"]+"  |  "+board["e7"]+"  |  "+board["e8"]+"  |  "+board["e9"]+"  |  "+board["e10"]+"  ")
+    print(" E |  "+board_user["e1"]+"  |  "+board_user["e2"]+"  |  "+board_user["e3"]+"  |  "+board_user["e4"]+"  |  "+board_user["e5"] +
+          "  |  " + board_user["e6"]+"  |  "+board_user["e7"]+"  |  "+board_user["e8"]+"  |  "+board_user["e9"]+"  |  "+board_user["e10"]+"  ")
     print(hor_vert)
     print(vert)
-    print(" F |  "+board["f1"]+"  |  "+board["f2"]+"  |  "+board["f3"]+"  |  "+board["f4"]+"  |  "+board["f5"] +
-          "  |  "+board["f6"]+"  |  "+board["f7"]+"  |  "+board["f8"]+"  |  "+board["f9"]+"  |  "+board["f10"]+"  ")
+    print(" F |  "+board_user["f1"]+"  |  "+board_user["f2"]+"  |  "+board_user["f3"]+"  |  "+board_user["f4"]+"  |  "+board_user["f5"] +
+          "  |  "+board_user["f6"]+"  |  "+board_user["f7"]+"  |  "+board_user["f8"]+"  |  "+board_user["f9"]+"  |  "+board_user["f10"]+"  ")
     print(hor_vert)
     print(vert)
-    print(" G |  "+board["g1"]+"  |  "+board["g2"]+"  |  "+board["g3"]+"  |  "+board["g4"]+"  |  "+board["g5"] +
-          "  |  " + board["g6"]+"  |  "+board["g7"]+"  |  "+board["g8"]+"  |  "+board["g9"]+"  |  "+board["g10"]+"  ")
+    print(" G |  "+board_user["g1"]+"  |  "+board_user["g2"]+"  |  "+board_user["g3"]+"  |  "+board_user["g4"]+"  |  "+board_user["g5"] +
+          "  |  " + board_user["g6"]+"  |  "+board_user["g7"]+"  |  "+board_user["g8"]+"  |  "+board_user["g9"]+"  |  "+board_user["g10"]+"  ")
     print(hor_vert)
     print(vert)
-    print(" H |  "+board["h1"]+"  |  "+board["h2"]+"  |  "+board["h3"]+"  |  "+board["h4"]+"  |  "+board["h5"] +
-          "  |  " + board["h6"]+"  |  "+board["h7"]+"  |  "+board["h8"]+"  |  "+board["h9"]+"  |  "+board["h10"]+"  ")
+    print(" H |  "+board_user["h1"]+"  |  "+board_user["h2"]+"  |  "+board_user["h3"]+"  |  "+board_user["h4"]+"  |  "+board_user["h5"] +
+          "  |  " + board_user["h6"]+"  |  "+board_user["h7"]+"  |  "+board_user["h8"]+"  |  "+board_user["h9"]+"  |  "+board_user["h10"]+"  ")
     print(hor_vert)
     print(vert)
-    print(" I |  "+board["i1"]+"  |  "+board["i2"]+"  |  "+board["i3"]+"  |  "+board["i4"]+"  |  "+board["i5"] +
-          "  |  " + board["i6"]+"  |  "+board["i7"]+"  |  "+board["i8"]+"  |  "+board["i9"]+"  |  "+board["i10"]+"  ")
+    print(" I |  "+board_user["i1"]+"  |  "+board_user["i2"]+"  |  "+board_user["i3"]+"  |  "+board_user["i4"]+"  |  "+board_user["i5"] +
+          "  |  " + board_user["i6"]+"  |  "+board_user["i7"]+"  |  "+board_user["i8"]+"  |  "+board_user["i9"]+"  |  "+board_user["i10"]+"  ")
     print(hor_vert)
     print(vert)
-    print(" J |  "+board["j1"]+"  |  "+board["j2"]+"  |  "+board["j3"]+"  |  "+board["j4"]+"  |  "+board["j5"] +
-          "  |  " + board["j6"]+"  |  "+board["j7"]+"  |  "+board["j8"]+"  |  "+board["j9"]+"  |  "+board["j10"]+"  ")
+    print(" J |  "+board_user["j1"]+"  |  "+board_user["j2"]+"  |  "+board_user["j3"]+"  |  "+board_user["j4"]+"  |  "+board_user["j5"] +
+          "  |  " + board_user["j6"]+"  |  "+board_user["j7"]+"  |  "+board_user["j8"]+"  |  "+board_user["j9"]+"  |  "+board_user["j10"]+"  ")
+    print(hor_vert)
+
+
+def print_board_cpu():
+    print("   |  1  |  2  |  3  |  4  |  5  |  6  |  7  |  8  |  9  | 10  ")
+    print(hor_vert)
+    print(vert)
+    print(" A |  "+board_cpu["a1"]+"  |  "+board_cpu["a2"]+"  |  "+board_cpu["a3"]+"  |  "+board_cpu["a4"]+"  |  "+board_cpu["a5"]+"  |  " +
+          board_cpu["a6"]+"  |  "+board_cpu["a7"]+"  |  "+board_cpu["a8"]+"  |  "+board_cpu["a9"]+"  |  "+board_cpu["a10"]+"  ")
+    print(hor_vert)
+    print(vert)
+    print(" B |  "+board_cpu["b1"]+"  |  "+board_cpu["b2"]+"  |  "+board_cpu["b3"]+"  |  "+board_cpu["b4"]+"  |  "+board_cpu["b5"]+"  |  " +
+          board_cpu["b6"]+"  |  "+board_cpu["b7"]+"  |  "+board_cpu["b8"]+"  |  "+board_cpu["b9"]+"  |  "+board_cpu["b10"]+"  ")
+    print(hor_vert)
+    print(vert)
+    print(" C |  "+board_cpu["c1"]+"  |  "+board_cpu["c2"]+"  |  "+board_cpu["c3"]+"  |  "+board_cpu["c4"]+"  |  "+board_cpu["c5"]+"  |  " +
+          board_cpu["c6"]+"  |  "+board_cpu["c7"]+"  |  "+board_cpu["c8"]+"  |  "+board_cpu["c9"]+"  |  "+board_cpu["c10"]+"  ")
+    print(hor_vert)
+    print(vert)
+    print(" D |  "+board_cpu["d1"]+"  |  "+board_cpu["d2"]+"  |  "+board_cpu["d3"]+"  |  "+board_cpu["d4"]+"  |  "+board_cpu["d5"] +
+          "  |  " + board_cpu["d6"]+"  |  "+board_cpu["d7"]+"  |  "+board_cpu["d8"]+"  |  "+board_cpu["d9"]+"  |  "+board_cpu["d10"]+"  ")
+    print(hor_vert)
+    print(vert)
+    print(" E |  "+board_cpu["e1"]+"  |  "+board_cpu["e2"]+"  |  "+board_cpu["e3"]+"  |  "+board_cpu["e4"]+"  |  "+board_cpu["e5"] +
+          "  |  " + board_cpu["e6"]+"  |  "+board_cpu["e7"]+"  |  "+board_cpu["e8"]+"  |  "+board_cpu["e9"]+"  |  "+board_cpu["e10"]+"  ")
+    print(hor_vert)
+    print(vert)
+    print(" F |  "+board_cpu["f1"]+"  |  "+board_cpu["f2"]+"  |  "+board_cpu["f3"]+"  |  "+board_cpu["f4"]+"  |  "+board_cpu["f5"] +
+          "  |  "+board_cpu["f6"]+"  |  "+board_cpu["f7"]+"  |  "+board_cpu["f8"]+"  |  "+board_cpu["f9"]+"  |  "+board_cpu["f10"]+"  ")
+    print(hor_vert)
+    print(vert)
+    print(" G |  "+board_cpu["g1"]+"  |  "+board_cpu["g2"]+"  |  "+board_cpu["g3"]+"  |  "+board_cpu["g4"]+"  |  "+board_cpu["g5"] +
+          "  |  " + board_cpu["g6"]+"  |  "+board_cpu["g7"]+"  |  "+board_cpu["g8"]+"  |  "+board_cpu["g9"]+"  |  "+board_cpu["g10"]+"  ")
+    print(hor_vert)
+    print(vert)
+    print(" H |  "+board_cpu["h1"]+"  |  "+board_cpu["h2"]+"  |  "+board_cpu["h3"]+"  |  "+board_cpu["h4"]+"  |  "+board_cpu["h5"] +
+          "  |  " + board_cpu["h6"]+"  |  "+board_cpu["h7"]+"  |  "+board_cpu["h8"]+"  |  "+board_cpu["h9"]+"  |  "+board_cpu["h10"]+"  ")
+    print(hor_vert)
+    print(vert)
+    print(" I |  "+board_cpu["i1"]+"  |  "+board_cpu["i2"]+"  |  "+board_cpu["i3"]+"  |  "+board_cpu["i4"]+"  |  "+board_cpu["i5"] +
+          "  |  " + board_cpu["i6"]+"  |  "+board_cpu["i7"]+"  |  "+board_cpu["i8"]+"  |  "+board_cpu["i9"]+"  |  "+board_cpu["i10"]+"  ")
+    print(hor_vert)
+    print(vert)
+    print(" J |  "+board_cpu["j1"]+"  |  "+board_cpu["j2"]+"  |  "+board_cpu["j3"]+"  |  "+board_cpu["j4"]+"  |  "+board_cpu["j5"] +
+          "  |  " + board_cpu["j6"]+"  |  "+board_cpu["j7"]+"  |  "+board_cpu["j8"]+"  |  "+board_cpu["j9"]+"  |  "+board_cpu["j10"]+"  ")
     print(hor_vert)
---
+++ Board_Setup.py
@@ -0,0 +1,190 @@
+import Board as board
+import random
+# Carrier (c) - 5
+# Battleship (b) - 4
+# Cruiser (r) - 3
+# Submarine (s) - 3
+# Destroyer (d) - 2
+
+ships = ["Carrier", "Battleship", "Cruiser", "Submarine", "Destroyer"]
+lengths = [5, 4, 3, 3, 2]
+
+
+def random_coordinate():
+    random_coordinate = random.choice(board.rows) + str(random.randrange(1, 11))
+    return random_coordinate
+
+
+def random_direction():
+    return random.choice(["n", "e", "s", "w"])
+
+
+def direction_ship(ship_letter, length, ship_start, cpu=None):
+    ship_start_letter = ship_start[0]
+    ship_start_letter_idx = board.rows.index(ship_start_letter)
+
+    if cpu:
+        direction = random_direction()
+        while (not is_on_board(ship_start, length, direction)) or (is_overlapping(ship_start, length, direction, True)):
+            direction = random_direction()
+
+    else:
+        direction = str(input(
+            "Would you like to like the bow to point? North (N), East (E), South (S) or West (W)?\n Please pick a letter: ")).lower()
+        while direction not in ["n", "e", "s", "w"]:
+            direction = str(input("Please select N/E/S/W: ")).lower()
+
+        while (not is_on_board(ship_start, length, direction)) or (is_overlapping(ship_start, length, direction)):
+            direction = str(input(
+                "Sorry, this is not possible, the ship would be out of bounds or overlapping another ship.\n Please select N/E/S/W: ")).lower()
+            while direction not in ["n", "e", "s", "w"]:
+                direction = str(input("Please select N/E/S/W: ")).lower()
+
+    if cpu:
+        if direction == 'n':
+            for i in range(length):
+                coordinate = board.rows[ship_start_letter_idx - i] + ship_start[1:]
+                board.board_cpu[coordinate] = ship_letter
+        elif direction == 's':
+            for i in range(length):
+                coordinate = board.rows[ship_start_letter_idx + i] + ship_start[1:]
+                board.board_cpu[coordinate] = ship_letter
+        elif direction == "e":
+            for i in range(length):
+                coordinate = ship_start[0] + str(int(ship_start[1:]) + i)
+                board.board_cpu[coordinate] = ship_letter
+        elif direction == 'w':
+            for i in range(length):
+                coordinate = ship_start[0] + str(int(ship_start[1:]) - i)
+                board.board_cpu[coordinate] = ship_letter
+    else:
+        if direction == 'n':
+            for i in range(length):
+                coordinate = board.rows[ship_start_letter_idx - i] + ship_start[1:]
+                board.board_user[coordinate] = ship_letter
+        elif direction == 's':
+            for i in range(length):
+                coordinate = board.rows[ship_start_letter_idx + i] + ship_start[1:]
+                board.board_user[coordinate] = ship_letter
+        elif direction == "e":
+            for i in range(length):
+                coordinate = ship_start[0] + str(int(ship_start[1:]) + i)
+                board.board_user[coordinate] = ship_letter
+        elif direction == 'w':
+            for i in range(length):
+                coordinate = ship_start[0] + str(int(ship_start[1:]) - i)
+                board.board_user[coordinate] = ship_letter
+
+
+def is_on_board(ship_start, length, direction):
+    ship_start_letter = ship_start[0]
+    ship_start_letter_idx = board.rows.index(ship_start_letter)
+    coordinate_numbers = [i for i in range(1, 11)]
+    ship_start_number = int(ship_start[1])
+    ship_start_number_idx = coordinate_numbers.index(ship_start_number)
+    if direction == 'n':
+        if ship_start_letter_idx - (length - 1) < 0:
+            return False
+        return True
+    elif direction == 's':
+        if ship_start_letter_idx + (length - 1) > 9:
+            return False
+        return True
+    elif direction == "e":
+        if ship_start_number_idx + (length - 1) > 9:
+            return False
+        return True
+    elif direction == "w":
+        if ship_start_number_idx - (length - 1) < 0:
+            return False
+        return True
+
+
+def is_overlapping(ship_start, length, direction=None, cpu=None):
+    ship_start_letter = ship_start[0]
+    ship_start_letter_idx = board.rows.index(ship_start_letter)
+
+    if cpu:
+        if direction == 'n':
+            for i in range(length):
+                coordinate = board.rows[ship_start_letter_idx - i] + ship_start[1]
+                if board.board_cpu[coordinate] != " ":
+                    return True
+            return False
+        elif direction == 's':
+            for i in range(length):
+                coordinate = board.rows[ship_start_letter_idx + i] + ship_start[1]
+                if board.board_cpu[coordinate] != " ":
+                    return True
+            return False
+        elif direction == "e":
+            for i in range(length):
+                coordinate = ship_start[0] + str(int(ship_start[1]) + i)
+                if board.board_cpu[coordinate] != " ":
+                    return True
+            return False
+        elif direction == 'w':
+            for i in range(length):
+                coordinate = ship_start[0] + str(int(ship_start[1]) - i)
+                if board.board_cpu[coordinate] != " ":
+                    return True
+            return False
+        else:
+            if board.board_cpu[ship_start] == " ":
+                return False
+            return True
+    else:
+        if direction == 'n':
+            for i in range(length):
+                coordinate = board.rows[ship_start_letter_idx - i] + ship_start[1]
+                if board.board_user[coordinate] != " ":
+                    return True
+            return False
+        elif direction == 's':
+            for i in range(length):
+                coordinate = board.rows[ship_start_letter_idx + i] + ship_start[1]
+                if board.board_user[coordinate] != " ":
+                    return True
+            return False
+        elif direction == "e":
+            for i in range(length):
+                coordinate = ship_start[0] + str(int(ship_start[1]) + i)
+                if board.board_user[coordinate] != " ":
+                    return True
+            return False
+        elif direction == 'w':
+            for i in range(length):
+                coordinate = ship_start[0] + str(int(ship_start[1]) - i)
+                if board.board_user[coordinate] != " ":
+                    return True
+            return False
+        else:
+            if board.board_user[ship_start] == " ":
+                return False
+            return True
+
+
+def place_ship(shipname, length, cpu=None):
+    if not cpu:
+        question = "Where would you like to place the stern of the {one}? Please select one of the coordinates\n".format(
+            one=shipname)
+        ship_start = str(input(question)).lower()
+        while ship_start not in board.board_user.keys() or is_overlapping(ship_start, 1):
+            ship_start = input(
+                "This is not a valid coordinate or this coordinate is already taken. Please select another one\n")
+        direction_ship(shipname[0], length, ship_start)
+    else:
+        ship_start = random_coordinate()
+        while is_overlapping(ship_start, 1, None, True):
+            ship_start = random_coordinate()
+        direction_ship(shipname[0], length, ship_start, True)
+
+
+def board_setup():
+    # board.print_board_user()
+    # for i in range(5):
+    #    place_ship(ships[i], lengths[i])
+    #    board.print_board_user()
+
+    for i in range(5):
+        place_ship(ships[i], lengths[i], True)
